99a74643b4484a8ca73fe0628737824505f86283
Rupesh Thakur <22rupeshthakur@gmail.com>
2026-09-15 13:03:44 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
-  },
-  nitro: {
-    preset: "netlify",
+    server: { 
+      entry: "server",
+      preset: "netlify" 
+    },
   },
 });
